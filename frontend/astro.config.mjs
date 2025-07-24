097481dbc6f7b4f51372e9192b71388b67fde651
Sergio Sánchez <49963862+jovialser@users.noreply.github.com>
2025-07-24 09:53:24 -0300

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,4 +4,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 integrations: [react()],
+  vite: {
+    build: {
+      assetsInlineLimit: 0 // 👈 fuerza que las imágenes se generen como archivos
+    }
+  }
 });
+
