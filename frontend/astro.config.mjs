5dffad4df47013b1aed39e6e43bd90f7f18b70d3
Sergio Sánchez <49963862+jovialser@users.noreply.github.com>
2025-07-25 13:09:43 -0300

    Update astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-integrations: [react()],
-  output: 'server' // Recomendado para Vercel
+  output: 'server',
+  // El adaptador de Vercel para Server-Side Rendering
+  adapter: vercel(),
+  integrations: [react()]
 });
 
